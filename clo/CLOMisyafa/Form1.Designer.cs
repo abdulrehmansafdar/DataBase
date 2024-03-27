bf16ece3f9183601f22fd4fe41c0e4580e7a3bb3
AmirHashmi191 <165129113+AmirHashmi191@users.noreply.github.com>
2024-03-27 01:22:49 -0700

    Add files via upload

---
+++ clo/CLOMisyafa/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CLOMisyafa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CLOName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CLOGrid = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CLOGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CLOName
+            // 
+            this.CLOName.Location = new System.Drawing.Point(269, 70);
+            this.CLOName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CLOName.Name = "CLOName";
+            this.CLOName.Size = new System.Drawing.Size(248, 22);
+            this.CLOName.TabIndex = 5;
+            this.CLOName.TextChanged += new System.EventHandler(this.CLOName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(198, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ManageCLO";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "ADD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CLOGrid
+            // 
+            this.CLOGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.CLOGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CLOGrid.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CLOGrid.Location = new System.Drawing.Point(12, 177);
+            this.CLOGrid.Name = "CLOGrid";
+            this.CLOGrid.RowHeadersWidth = 51;
+            this.CLOGrid.RowTemplate.Height = 24;
+            this.CLOGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.CLOGrid.Size = new System.Drawing.Size(776, 261);
+            this.CLOGrid.TabIndex = 8;
+            this.CLOGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(269, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(218, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "MODIFY";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(533, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(218, 36);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "DELETE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.CLOGrid);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CLOName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CLOGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CLOName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView CLOGrid;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ clo/CLOMisyafa/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CLOMisyafa
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ADDing function
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constr = "Data Source=GREY\\SQLEXPRESS;Initial Catalog=ProjectB;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into CLo values(@CLOName, GetDate(), GetDate())", con);
+            cmd.Parameters.AddWithValue("@CLOName", CLOName.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Inserted!");
+            printCLO();
+        }
+
+        // deletion
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int rowIndex = Convert.ToInt32(CLOGrid.SelectedRows[0].Cells[0].Value);
+            string connectionString = "Data Source=Grey\\SQLEXPRESS;Initial Catalog=ProjectB;Integrated Security=True";
+            string deleteQuery = "DELETE FROM CLo WHERE id = @CLoId";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(deleteQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@CloId", rowIndex);
+
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Successfully Deleted!");
+                        printCLO();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                }
+            }
+
+        }
+
+        
+        // function used for printing clo
+        private void printCLO()
+        {
+            string connectionString = "Data Source=GREY\\SQLEXPRESS;Initial Catalog=ProjectB;Integrated Security=True";
+            string query = "SELECT * FROM CLo";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable dataTable = new DataTable();             
+                dataTable.Load(reader);
+                CLOGrid.DataSource = dataTable;
+                reader.Close();
+                connection.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                printCLO();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        // modification
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int CLoID = Convert.ToInt32(CLOGrid.SelectedRows[0].Cells[0].Value); // gets the id of selected row
+            DateTime Date = Convert.ToDateTime(CLOGrid.SelectedRows[0].Cells[2].Value); // gets the date from selected row
+
+            string constr = "Data Source=GREY\\SQLEXPRESS;Initial Catalog=ProjectB;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update CLo SET Name=@Name,DateCreated = @dateCreated ,DateUpdated = GetDate() Where ID=@ID", con);
+            cmd.Parameters.AddWithValue("@Name", CLOName.Text);
+            cmd.Parameters.AddWithValue("@dateCreated", Date);
+            cmd.Parameters.AddWithValue("@ID", CLoID);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("CLO updated successfully", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            printCLO();
+
+            
+        }
+
+        private void CLOName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
